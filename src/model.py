1324d595d66825b9e86eb84230f9b929bb442bf8
Min Yan <>
2023-06-13 15:29:23 +0800

    Simplify Model predict() for singular predictions

--- src/model.py
+++ src/model.py
@@ -5,8 +5,5 @@ class Model:
     def __init__(self):
         self.model = joblib.load('model/catboost_model.pkl')
 
-    def predict(self, inputs):
-        import numpy as np
-        input_features = np.array([inputs], dtype='object')
-        print(input_features)
-        return self.model.predict(input_features)[0]
+    def predict(self, input_features):
+        return self.model.predict(input_features)
